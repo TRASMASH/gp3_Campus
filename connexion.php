f53b06e263887b75ad9e52d877e7d4d58116d4ce
Younes LOUREIRO <ylour@lmdsio.com>
2026-03-03 09:11:28 +0100

    Deuxieme  commit : Initialisation du projet PHP

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<?php
+class Connexion {
+    private static $cnx = null;
+    public static function getConnexion() {
+        $dbhost = '127.0.0.1';
+        $dbbase = 'personnel';
+        $dbuser = 'usersio';
+        $dbpwd = 'sio';
+        try {
+            self::$cnx = new PDO("mysql:host=$dbhost;dbname=$dbbase", $dbuser, $dbpwd);
+            self::$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$cnx->exec('SET CHARACTER SET utf8');
+        } catch (PDOException $e) {
+            $erreur = $e->getMessage();
+        } return self::$cnx;
+}
+    public static function deConnexion(){
+        try { self::$cnx = null;
+    }
+    catch (PDOException $e) {
+        $erreur = $e->getMessage(); }
+    }
+}
